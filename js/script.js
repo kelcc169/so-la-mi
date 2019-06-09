946e9f35332cc48d146346196a396b446b0ce1b7
Kelsey Cox <kelseycox@Lucy.local>
2019-06-09 10:42:59 -0700

    game logic and images

--- js/script.js
+++ js/script.js
@@ -1,38 +1,44 @@
 //Variables
-    //turn counter
 var playerInput = [];
-var computer = 'misola';
+var solfege = 'misola';
 
 //DOM Associations
-    //player scores
+var scoreOne;
+var scoreTwo;
     //reset button
-var clickbox = document.getElementById('clickbox');
     //current player
     //sounds to play
-
-//Event Listeners
-    //make sure to do DOMContentLoaded
-    //solfege button clicks
-    //start/reset clicks
-    //hover to play notes, if time
-clickbox.addEventListener('click', function(e) {
-    if (e.target.id !== 'clickbox') {
-        playerInput.push(e.target.id);
-        var string = playerInput.join('');
-        console.log(string);
-        if (string.length === 6) {
-            if (string === computer) {
-                console.log('match')
-            } else {
-                console.log('not a match');
-            }
-            playerInput = [];
+    
+    //Event Listeners
+document.addEventListener('DOMContentLoaded', function (e) {
+    var clickbox = document.getElementById('clickbox');
+    var scoreOne = document.getElementById('player1');
+    var scoreTwo = document.getElementById('player2');
+    
+    clickbox.addEventListener('click', function(e) {
+        if (e.target.id !== 'clickbox') {
+            playerInput.push(e.target.id);
+            var playerNotes = playerInput.join('');
+            
+            checkMatch(playerNotes);
         }
-    }
+    });
 });
+//start/reset clicks
+//hover to play notes, if time
 
 
 //Functions
-    //randomizer for selection of notes
-    //reset game
-    //
+//randomizer for selection of notes
+//reset game
+function checkMatch (notes) {
+    if (notes.length === 6) {
+        if (notes === solfege) {
+            console.log('match')
+        } else {
+            console.log('not a match');
+        }
+        playerInput = [];
+    }
+}
+    
