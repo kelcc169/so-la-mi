da8787ccd874fa74a4543008594c53c931759325
Kelsey Cox <kelseycox@Lucy.local>
2019-06-09 14:32:03 -0700

    game logic

--- js/script.js
+++ js/script.js
@@ -1,44 +1,82 @@
 //Variables
+var solfegeBank = ['mi', 'so', 'la'];
 var playerInput = [];
-var solfege = 'misola';
+var solfege = [];
+var oneScore = 0;
+var scoreTwo = 0;
 
 //DOM Associations
-var scoreOne;
-var scoreTwo;
+var oneScoreBox;
+var twoScoreBox;
+var clickbox;
+var startBtn;
+var currentPl = 'one';
     //reset button
-    //current player
     //sounds to play
     
     //Event Listeners
 document.addEventListener('DOMContentLoaded', function (e) {
-    var clickbox = document.getElementById('clickbox');
-    var scoreOne = document.getElementById('player1');
-    var scoreTwo = document.getElementById('player2');
+    clickbox = document.getElementById('clickbox');
+    oneScoreBox = document.getElementById('player1');
+    twoScoreBox = document.getElementById('player2');
+    startBtn = document.getElementById('start');
+    currentPl = document.getElementById('currentPl');
+    
+    startBtn.addEventListener('click', function (e) {
+        randomNotes();
+        console.log(solfege)
+    })
     
     clickbox.addEventListener('click', function(e) {
         if (e.target.id !== 'clickbox') {
             playerInput.push(e.target.id);
             var playerNotes = playerInput.join('');
-            
+
             checkMatch(playerNotes);
         }
     });
+
+    //reset clicks
+    
 });
-//start/reset clicks
 //hover to play notes, if time
 
 
 //Functions
 //randomizer for selection of notes
+function randomNotes () {
+    let i = 0;
+    while (i < 3) {
+        var item = solfegeBank[Math.floor(Math.random()*solfegeBank.length)];
+        solfege.push(item);
+        i++;
+    }
+
+}
+//game over
+
 //reset game
+// function reset
+
+//start game
+// function startGame (e) {
+    //call randomizer
+    //play the sounds
+// }
+
+//check for a match and change scores
 function checkMatch (notes) {
+    var answer = solfege.join('');
     if (notes.length === 6) {
-        if (notes === solfege) {
-            console.log('match')
+        if (notes === answer){
+            console.log('match');
+            oneScore++;
+            oneScoreBox.textContent = oneScore;
         } else {
             console.log('not a match');
         }
         playerInput = [];
+        solfege = [];
     }
-}
+};
     
